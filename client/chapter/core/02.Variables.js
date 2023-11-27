ba513187906c292a1a47bfb74b53db929c108988
yen815 <chldpdms815@gmail.com>
2023-11-27 22:16:09 +0900

    datatype, operation js 연습

---
+++ client/chapter/core/02.Variables.js
@@ -0,0 +1,50 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+let admin;
+let name;
+
+name = 'john';
+
+admin = name;
+
+// alert(admin);
+
+// console.log(admin);
+
+const ourPlanetName = 'Earth';
+// console.log(ourPlanetName);
+
+const currentOnlineUserName = 'tiger';
+// console.log(currentOnlineUserName);
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+// - 갯수 별 상품 가격 계산하기
+let calcProductPriceByQuantity;
+
+// - 구매 제품 가격의 총 합
+let totalProductPrice;
+
+// - 1년 기준 일(day)자 수
+const DAY_PER_YEAR = 365;
+
+// - 구매 결제 여부
+let isPayment = true;
+let hasCartList = false;
+
+// - 구매 결제 내역
+let paymentList;
+
+// - 브랜드 접두사
+const BRAND_PREFIX = 'NIKE';
+
+// - 오늘의 운세
+let fortuneOfToday = 'good';
+
+// - 상품 정보
+const productInformation = 'good';
+
+/* variables ----------------------------------------------------------- */
+
+/* constant variables -------------------------------------------------- */
