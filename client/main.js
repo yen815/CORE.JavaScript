61f81f930e3651338a720d902e1ee1fd5a45dca5
yen815 <chldpdms815@gmail.com>
2023-12-18 15:41:40 +0900

    module

--- client/main.js
+++ client/main.js
@@ -1,3 +1,72 @@
-const a = 'hello';
+// 모듈 프로그래밍
+// named export(이름 내보내기) -> {이름}
+// default export(기본 내보내기) -> 이름 : 해당 파일에 딱 하나만 보낼 수 있다.
 
-console.log(navigator.language);
+// import { getNode } from './lib/dom/getNode.js';
+// import { insertLast } from './lib/dom/insert.js';
+// import clearContents from './lib/dom/clear.js';
+
+import { getNode, clearContents, insertLast, getNodes } from './lib/index.js';
+
+function phase1() {
+  const first = getNode('#firstNumber');
+  const second = getNode('#secondNumber');
+  const result = getNode('.result');
+  const clear = getNode('.#clear');
+
+  // 1. input value 값 가져오기
+  //     - input에게 input 이벤트를 걸어 준다.
+  //     - input.value 로 가져온다.
+
+  // 2. 두 수의 합 더하기
+  //     - 타입 확인하기.
+
+  // 3. 합계 랜더링 하기
+  //     - insertLast(), insertAdjacentHTML()
+
+  function handleInput() {
+    const firstValue = first.value * 1;
+    const secondValue = second.value / 1;
+    const total = firstValue + secondValue;
+
+    // result.textContent = ''
+
+    clearContents(result);
+    insertLast(result, total);
+  }
+
+  function handleClear(e) {
+    e.preventDefault();
+    clearContents(first);
+    clearContents(second);
+    result.textContent = '-';
+  }
+
+  first.addEventListener('input', handleInput);
+  second.addEventListener('input', handleInput);
+  clear.addEventListener('click', handleClear);
+}
+
+// 이벤트 위임 처리
+const calculator = getNode('.calculator');
+const result = getNode('.result');
+const clear = getNode('#clear');
+const numberInputs = Array.from(getNodes('input:not(#clear)'));
+
+function handleInput() {
+  const total = numberInputs.reduce((acc, cur) => acc + Number(cur.value), 0);
+
+  clearContents(result);
+  insertLast(result, total);
+}
+
+function handleClear(e) {
+  e.preventDefault();
+
+  numberInputs.forEach(clearContents); // 자동 전달
+  // numberInputs.forEach((item) => clearContents(item)); // 자동 전달
+  result.textContent = '-';
+}
+
+calculator.addEventListener('input', handleInput);
+clear.addEventListener('click', handleClear);
