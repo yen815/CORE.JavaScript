4680503a9e9ee7032fa1a49b9c285ba51f889ce9
yen815 <chldpdms815@gmail.com>
2023-12-18 13:43:40 +0900

    수정

---
+++ client/chapter/dom/09.delegation.js
@@ -0,0 +1,30 @@
+/* ------------------------ */
+/* Event delegation         */
+/* ------------------------ */
+
+/* 클래스를 사용한 위임 ---------------- */
+
+const nav = getNode('nav');
+
+function handleClick(e) {
+  let target = e.target;
+  let li = target.closest('li');
+
+  if (!li) return;
+
+  // let dataName = attr(li,'data-name');
+
+  // if(li.className === 'home'){
+  //   li.style.background = 'orange';
+  // }
+
+  // if (dataName === 'about') li.style.background = 'hotpink';
+  // if (dataName === 'home') li.style.background = 'greenyellow';
+  // if (dataName === 'contact') li.style.background = 'dodgerblue';
+}
+
+nav.addEventListener('click', handleClick);
+
+/* 속성을 사용한 위임 ------------------ */
+
+/* 노드를 사용한 위임 ------------------ */
